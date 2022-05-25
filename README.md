8e874d79657b014973c10d389818d7a18f6c65a0
Alexander Erben <info@it-erben.com>
2022-05-25 17:26:50 +0200

    Implement tool

---
+++ README.md
@@ -0,0 +1,14 @@
+# Time to wake up the neighborhood maker
+
+
+![Time to wake up the neighborhood](example.png "Time to wake up the neighborhood")
+
+A tool that renders an album cover on top of a "time to wake up the neighborhood" template image.
+
+## build
+
+``go build .``
+
+## Usage:
+
+``./wakeup -j $PATH_TO_ALBUM_COVER -o $PATH_TO_OUT_FILE [-t $PATH_TO_TEMPLATE_FILE]``
---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/disintegration/imaging"
+	"github.com/nfnt/resize"
+	"image"
+	"image/color"
+	"image/draw"
+	_ "image/jpeg"
+	"image/png"
+	"os"
+)
+
+func main() {
+
+	templateFileFlag := flag.String("t", "./resources/template.jpg", "template file path")
+	overlayFileFlag := flag.String("l", "./resources/overlay.png", "overlay file path")
+	outputFileFlag := flag.String("o", "./output.png", "output file path")
+	flag.Parse()
+
+	templateSrc := loadTemplate(*templateFileFlag)
+	overlaySrc := loadTemplate(*overlayFileFlag)
+
+	scaledOverlay := resize.Resize(70, 0, overlaySrc, resize.Lanczos3)
+	rotated := imaging.Rotate(scaledOverlay, 60, color.Transparent)
+
+	for i := 1; i < 13; i++ {
+		drawCircle(rotated, 5, 40, i, color.Transparent)
+	}
+
+	target := image.NewNRGBA(templateSrc.Bounds())
+	draw.Draw(target, templateSrc.Bounds(), templateSrc, image.Point{X: 0, Y: 0}, draw.Src)
+	offset := image.Point{X: -265, Y: -180}
+	draw.Draw(target, image.Rect(0, 0, 330, target.Bounds().Dy()), rotated, offset, draw.Over)
+
+	writePng(*outputFileFlag, target)
+}
+
+func writePng(path string, target *image.NRGBA) {
+	out, err := os.Create(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	encodeErr := png.Encode(out, target)
+	if encodeErr != nil {
+		panic(encodeErr.Error())
+	}
+}
+
+func loadTemplate(path string) image.Image {
+	infile, err := os.Open(path)
+	defer func(infile *os.File) {
+		err := infile.Close()
+		if err != nil {
+			panic(err.Error())
+		}
+	}(infile)
+	if err != nil {
+		panic(err.Error())
+	}
+	src, _, err := image.Decode(infile)
+	if err != nil {
+		panic(err.Error())
+	}
+	return src
+}
+
+func drawCircle(img draw.Image, x0, y0, r int, c color.Color) {
+	x, y, dx, dy := r-1, 0, 1, 1
+	err := dx - (r * 2)
+
+	for x > y {
+		img.Set(x0+x, y0+y, c)
+		img.Set(x0+y, y0+x, c)
+		img.Set(x0-y, y0+x, c)
+		img.Set(x0-x, y0+y, c)
+		img.Set(x0-x, y0-y, c)
+		img.Set(x0-y, y0-x, c)
+		img.Set(x0+y, y0-x, c)
+		img.Set(x0+x, y0-y, c)
+
+		if err <= 0 {
+			y++
+			err += dy
+			dy += 2
+		}
+		if err > 0 {
+			x--
+			dx += 2
+			err += dx - (r * 2)
+		}
+	}
+}
